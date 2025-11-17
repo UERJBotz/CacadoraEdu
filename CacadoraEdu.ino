66100cf817e1adfbf7ded64ca50f27bc64443497
diogonascimento1 <diogoalvesnascimento23@gmail.com>
2025-11-17 11:07:58 -0300

    WIP: zigzag diogo

--- CacadoraEdu.ino
+++ CacadoraEdu.ino
@@ -1,5 +1,3 @@
-//! ver se os sensores da esquerda estão na direita e vice versa (incluindo frente)(acho que tão)
-
 #include "edu.h"
 #include "SumoIR.h"
 #include "LEDFX.h"
@@ -14,6 +12,11 @@ enum simbolo {
     DIR,
     FRENTE_ESQ,
     FRENTE_DIR,
+
+    TEMP0,
+    TEMP1,
+    TEMP2,
+    TEMP3,
 };
 enum estado {
     G_ESQ,
@@ -26,10 +29,13 @@ enum estrategia {
     PIAO = 4,
     MADMAX = 5,
     MADMAX_ESQUERDA = 6,
+    ZIGZAG = 7,
 };
 
 enum estado estado_atual;
 enum estrategia estrategia = PIAO;
+unsigned long tempo_anterior = 0;
+
 
 void setup() { 
     init_edu(9600);
@@ -37,66 +43,73 @@ void setup() {
     IR.setLed(2,HIGH,180);
 }
 
-
 void loop() {  
     IR.update();
-
+    unsigned long dt = millis() - tempo_anterior;
     struct leitura sensores = leitura_sensores();
     mostra_sensores_no_led(sensores);
 
-    if (IR.available()) { /* quando o sensor tiver ativado */
-        int cmd = IR.read(); // salva o número lido pelo sensor, estando ou não de 1 a 3
+    // Recebe comandos via controle IR
+    if (IR.available()) {
+        int cmd = IR.read();
         if (cmd >= 4 && cmd <= 9) {
             estrategia = (enum estrategia)cmd; // substitui a estratégia atual por esse número
             mover(0,0); mostra_estrategia_no_led(estrategia); delay(100);
         }
     }
 
-    if (IR.prepare()) { /* robô em preparação */
+    if (IR.prepare()) { // Estado de preparação
         Serial.println("Preparar");
         estado_atual = G_DIR;
-        mover(0,0);
-
-    } else if (IR.start()) {
+        mover(0, 0);
+    } else if (IR.start()) { // Início da luta
         Serial.println("-> sumo start");
+    } else if (IR.on()) { // Robô ligado e em combate
+        enum simbolo simb; // visível em todo o bloco abaixo
 
-    } else if (IR.on()) {
-        enum simbolo simb;
-
-        // retorna o símbolo adequado de acordo com os sensores
         if      (sensores.frente_esq &&
                  sensores.frente_dir) simb = FRENTE;
         else if (sensores.esq)        simb = ESQ;
         else if (sensores.dir)        simb = DIR;
         else if (sensores.frente_esq) simb = FRENTE_ESQ;
         else if (sensores.frente_dir) simb = FRENTE_DIR;
+        else if (dt < 3000)           simb = TEMP0;
+        else if (dt < 5000)           simb = TEMP1;
+        else if (dt < 8000)           simb = TEMP2;
+        else if (dt < 10000)          simb = TEMP3;
         else                          simb = NADA;
 
         switch (estrategia) {
+            default:
             case PIAO: {
                 estado_atual = prox_estado(estado_atual, simb);
                 acao_atual(estado_atual);
             } break;
 
-            case MADMAX: { 
+            case MADMAX: {
                 Serial.println("FAZENDO MADMAX");
                 mover(1023, 1023);
-                //mostra_estrategia_no_led(9);
             } break;
+
             case MADMAX_ESQUERDA: {
                 mover(-1023, 1023);
                 mostra_estrategia_no_led(9);
             } break;
-        }
 
-    } else if (IR.stop()){
+            case ZIGZAG: {
+                estado_atual = estado_zigzag(estado_atual, simb);
+                acao_atual(estado_atual,simb);
+            } break;
+        }
+    } else if (IR.stop()) { // Parada
         Serial.println("-> sumo stop");
-        mover(0,0);
+        mover(0, 0);
     }
 }
 
+
 // Define os próximos estados com base no estado atual e símbolo lido
-estado prox_estado(estado e, simbolo s) {
+enum estado prox_estado(enum estado e, enum simbolo s) {
     switch (e) {
         case G_DIR:
             switch (s) {
@@ -148,6 +161,88 @@ estado prox_estado(estado e, simbolo s) {
     return e; // valor padrão de segurança
 }
 
+
+enum estado estado_zigzag(enum estado f, enum simbolo s) {
+    switch (f) {
+        case G_DIR:
+            switch (s) {
+                case TEMP0:      return G_DIR;
+                case TEMP1:      return RETO;
+                case TEMP2:      return G_ESQ;
+                case TEMP3:      return RETO;
+                case FRENTE:     return RETO;
+                
+                case ESQ:        return G_ESQ;
+                case FRENTE_ESQ: return G_FRENTE_ESQ;
+                case FRENTE_DIR: return G_FRENTE_DIR;
+                case DIR:        return G_DIR;
+                case NADA:       return G_DIR;
+            }
+            break;
+
+        case G_ESQ:
+            switch (s) {
+                case TEMP0:      return G_DIR;
+                case TEMP1:      return RETO;
+                case TEMP2:      return G_ESQ;
+                case TEMP3:      return RETO;
+                case FRENTE:     return RETO;
+                case ESQ:        return G_ESQ;
+                case FRENTE_ESQ: return G_FRENTE_ESQ;
+                case FRENTE_DIR: return G_FRENTE_DIR;
+                case DIR:        return G_DIR;
+                case NADA:       return G_DIR;
+            }
+            break;
+
+        case RETO:
+            switch (s) {
+                case TEMP0:      return G_DIR;
+                case TEMP1:      return RETO;
+                case TEMP2:      return G_ESQ;
+                case TEMP3:      return RETO;
+                case FRENTE:     return RETO;
+                case ESQ:        return G_ESQ;
+                case FRENTE_ESQ: return G_FRENTE_ESQ;
+                case FRENTE_DIR: return G_FRENTE_DIR;
+                case DIR:        return G_DIR;
+                case NADA:       return G_DIR;
+        }
+        break;
+
+    case G_FRENTE_DIR:
+        switch (s) {
+            case TEMP0:      return G_DIR;
+            case TEMP1:      return RETO;
+            case TEMP2:      return G_ESQ;
+            case TEMP3:      return RETO;
+            case FRENTE:     return RETO;
+            case ESQ:        return G_ESQ;
+            case FRENTE_ESQ: return G_FRENTE_ESQ;
+            case FRENTE_DIR: return G_FRENTE_DIR;
+            case DIR:        return G_DIR;
+            case NADA:       return G_DIR;
+        }
+        break;
+    case G_FRENTE_ESQ:
+        switch (s) {
+            case TEMP0:      return G_DIR;
+            case TEMP1:      return RETO;
+            case TEMP2:      return G_ESQ;
+            case TEMP3:      return RETO;
+            case FRENTE:     return RETO;
+            case ESQ:        return G_ESQ;
+            case FRENTE_ESQ: return G_FRENTE_ESQ;
+            case FRENTE_DIR: return G_FRENTE_DIR;
+            case DIR:        return G_DIR;
+            case NADA:       return G_DIR;
+        }
+        break;
+    }
+
+    return f;  // retorno seguro
+}
+
 void acao_atual(estado e) {
     switch (e) {
         case RETO: {
@@ -172,4 +267,3 @@ void acao_atual(estado e) {
         } break;
     }
 }
-   
